0a3dea9dfda7b4244b86d00dd02b8abe7b14bccb
UHDFox <108611791+UHDFox@users.noreply.github.com>
2024-07-16 15:15:38 +0300

    Add ClassicUI tab

---
+++ SpeedTool/Windows/Settings/Tabs/ClassicUI.cs
@@ -0,0 +1,23 @@
+using System.Numerics;
+using ImGuiNET;
+using SpeedTool.Util.ImGui;
+
+namespace SpeedTool.Windows.Settings.Tabs;
+
+public sealed class ClassicUI : TabBase
+{
+    private Vector4 activeSplitColor = new Vector4(1);
+    private int ShownSplitsCount = 5;
+    
+    public override void DoTab()
+    {
+        if (ImGui.BeginTabItem("ClassicUI"))
+        {
+            ImGuiExtensions.SpeedToolColorPicker("Active split", ref activeSplitColor);
+            ImGui.Text("Shown splits");
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(75f);
+            ImGui.InputInt("", ref ShownSplitsCount);
+        }
+    }
+}
