df0382e39925741421e4342076a974ca5e98f647
Grim <grimmaple95@gmail.com>
2024-09-11 12:47:26 +0300

    Resolve #108 Add proper Lua sandboxing

--- InjectedTimer/ScriptEngine.cs
+++ InjectedTimer/ScriptEngine.cs
@@ -8,9 +8,10 @@ namespace InjectedTimer
     {
         public ScriptEngine(string script, Pipe sink)
         {
-            luaState = new Lua(false);
+            luaState = new Lua(true);
             ScriptFunction.Sink = sink;
-            luaState.DoString("import = function () end");
+            
+            luaState.DoString(SANDBOX);
 
             luaState.RegisterFunction("module_base_address", ((Func<string, long>)ScriptFunction.GetModuleAddress).GetMethodInfo());
             luaState.RegisterFunction("debug_message", ((Action<string>)ScriptFunction.DebugMessage).GetMethodInfo());
@@ -29,13 +30,13 @@ namespace InjectedTimer
             luaState.RegisterFunction("pointer_path", ((Func<long[], long>)ScriptFunction.PointerPath).GetMethodInfo());
 
             luaState.DoString(script);
-            luaState.DoString("on_load()");
+            luaState.DoString("run_sandbox(sandbox_env, on_load)");
             luaState.DoString("");
         }
 
         public void OnFrame()
         {
-            luaState.DoString("on_frame()");
+            luaState.DoString("run_sandbox(sandbox_env, on_frame)");
         }
 
         public void Dispose()
@@ -44,5 +45,51 @@ namespace InjectedTimer
         }
 
         Lua luaState;
+
+        private const string SANDBOX =
+"""
+-- save a pointer to globals that would be unreachable in sandbox
+local e=_ENV
+
+-- sample sandbox environment
+sandbox_env = {
+  ipairs = ipairs,
+  next = next,
+  pairs = pairs,
+  pcall = pcall,
+  tonumber = tonumber,
+  tostring = tostring,
+  type = type,
+  unpack = unpack,
+  coroutine = { create = coroutine.create, resume = coroutine.resume, 
+      running = coroutine.running, status = coroutine.status, 
+      wrap = coroutine.wrap },
+  string = { byte = string.byte, char = string.char, find = string.find, 
+      format = string.format, gmatch = string.gmatch, gsub = string.gsub, 
+      len = string.len, lower = string.lower, match = string.match, 
+      rep = string.rep, reverse = string.reverse, sub = string.sub, 
+      upper = string.upper },
+  table = { insert = table.insert, maxn = table.maxn, remove = table.remove, 
+      sort = table.sort },
+  math = { abs = math.abs, acos = math.acos, asin = math.asin, 
+      atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos, 
+      cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor, 
+      fmod = math.fmod, frexp = math.frexp, huge = math.huge, 
+      ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max, 
+      min = math.min, modf = math.modf, pi = math.pi, pow = math.pow, 
+      rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh, 
+      sqrt = math.sqrt, tan = math.tan, tanh = math.tanh },
+  os = { clock = os.clock, difftime = os.difftime, time = os.time },
+}
+
+function run_sandbox(sb_env, sb_func, ...)
+  local sb_orig_env=_ENV
+  if (not sb_func) then return nil end
+  _ENV=sb_env
+  local sb_ret={e.pcall(sb_func, ...)}
+  _ENV=sb_orig_env
+  return e.table.unpack(sb_ret)
+end
+""";
     }
 }
